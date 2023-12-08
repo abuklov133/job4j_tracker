ceb2526d105346b2966fabff862011ef47cecb46
abuklov133 <abuklov133@gmail.com>
2023-12-08 13:11:40 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,6 +1,14 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
+    private LocalDateTime created = LocalDateTime.now();
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
     private int id;
     private String name;
 
