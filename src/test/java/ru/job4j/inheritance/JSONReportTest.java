bd7a73d4366c568649d6f1922fea7833d66b8dd1
abuklov133 <abuklov133@gmail.com>
2023-12-25 10:37:04 +0300

    4. Переопределение метода [#174890 #465155]

--- src/test/java/ru/job4j/inheritance/JSONReportTest.java
+++ src/test/java/ru/job4j/inheritance/JSONReportTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class JSONReportTest {
-
     @Test
     public void whenTestGenerateMethod() {
         String ln = System.lineSeparator();
