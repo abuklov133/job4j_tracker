d3fa19ebb4e104600d8a2e6a15861f6580d143e9
abuklov133 <abuklov133@gmail.com>
2024-02-06 14:37:26 +0300

    4.2. Интерфейс Input [#181092 #470772]

---
+++ src/main/java/ru/job4j/tracker/polymorphism/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker.polymorphism;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+        System.out.println("Едет долго и не комфортно.");
+    }
+
+    @Override
+    public void traveler(int passenger) {
+        System.out.println("Перевозит " + passenger + " пассажиров.");
+    }
+
+    @Override
+    public double refuel(int fuel) {
+        double price = 100;
+        return fuel * price;
+    }
+}
