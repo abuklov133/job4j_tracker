8f68520d63515b1a9a93b31f75823a4742326993
abuklov133 <abuklov133@gmail.com>
2024-02-19 13:02:56 +0300

    9.1. Тестирование. Подготовка данных. [#182960 #480052]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -3,9 +3,6 @@ package ru.job4j.tracker;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 class StartUITest {
     @Test
@@ -13,8 +10,8 @@ class StartUITest {
         Tracker tracker = new Tracker();
         Item item = new Item("New item");
         tracker.add(item);
-        Item edited = tracker.findById(item.getId());
         tracker.delete(item.getId());
-        assertSame(tracker.findAll().length, 0);
+        Item actual = tracker.findById(item.getId());
+        assertThat(actual).isNull();
     }
 }
