a59ca55e8db8c275b1409d778fdcc43b7040f4a7
abuklov133 <abuklov133@gmail.com>
2024-02-20 09:38:01 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i] == key) {
+                result = i;
+            }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException("The index cannot be less then 0");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] value = {"A", "B", "C"};
+        try {
+            int index = indexOf(value, "E");
+            System.out.println(index);
+        } catch (ElementNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
